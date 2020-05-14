46bd1b4570902f10fa463f0e0633873cd8d662b4
Amanuel Getachew <amanuel.shiferaw-cons@nord-com.it>
2020-05-14 13:02:12 +0200

    save category parent child

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-
-const categorySchema = mongoose.Schema({
+const categorySchema = new Schema({
+    _id: {type: Schema.Types.ObjectId},
     title: String,
     description: String,
-    children: {type: Array, default: []},
+    posts: [{type: Schema.Types.ObjectId, ref: 'Post'}],
+    subCategories: [{type: Schema.Types.ObjectId, ref: 'Category'}],
     thumbUrl: String,
     createdAt: Date,
     modifiedAt: { type: Date, default: Date.now() }
--- src/repository/category.repository.js
+++ src/repository/category.repository.js
@@ -1,7 +1,10 @@
 const Category = require('../models/category.model');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 exports.create = (data, callback) => {
     const category = new Category({
+        _id: new mongoose.Types.ObjectId(),
         title: data.title,
         url: data.url,
         description: data.description,
@@ -16,11 +19,13 @@ exports.create = (data, callback) => {
             return;
         }
         //search category and add this as child
-        Category.findById({_id: data.parentCategoryId}, function(err, catObj) {
-            if(err) callback(err);
-            console.log(catObj._id);
-            category.parentCategoryId = catObj._id;
-        });
+        if (data.parentCategoryId) {
+            Category.findById({ _id: data.parentCategoryId }, function (err, catObj) {
+                if (err) callback(err);
+                catObj.subCategories.push(category._id);
+                catObj.save();
+            });
+        }
 
         console.log('Category saved successfully.')
         callback(null, postData);
